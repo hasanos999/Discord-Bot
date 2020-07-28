90fe93cb9defb311cac505413d03356d47657849
Glitch (hello-sqlite) <none>
2020-07-28 07:39:52 +0000

    📮🎹 Checkpoint ./komutlar/küfürengel.js:6374285/786 ./komutlar/mesajat.js:6374285/1267 ./komutlar/müzik.js:6374285/819 ./komutlar/otomesajkapa.js:6374285/1141 ./bot.js:6374285/2 ./util/Util.js:6374285/4932 ./util/eventLoader.js:6374285/136

---
+++ komutlar/müzik.js
@@ -0,0 +1,30 @@
+  const Discord = require("discord.js")
+
+exports.run = async(client, message) => {
+	
+	let prefix = '!'
+
+	const codare = new Discord.RichEmbed()
+       .setAuthor(`Komutlar`)
+       .setTitle(`Müzik Komutları`)
+       .setDescription(`!çal:\nYazdığınız Müziği Çalar\n\n!geç:\nÇalan Şarkıyı Atlar.\n\n!kapat:\nÇalmakta Olan Şarkıyı Kapatır.\n\n!ses:\nŞarkının Sesini Ayarlar.\n\n!çalan:\nÇalmakta Olan Şarkıyı Gösterir.\n\n!kuyruk:\nKuyruktaki Şarkıları Gösterir.\n\n!durdur:\nŞarkıyı Durdurur.\n\n!devam:\nŞarkıyı Devam Ettirir.`)
+
+  .setFooter(`Yapımcı @Hasan 14`)
+  return message.channel.send(codare)
+}
+
+
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+  kategori: `Geliştirici`
+};
+
+exports.help = {
+  name:'myardım' ,
+  description: '[Admin Komutu]',
+  usage: '!bakım-mod aç'
+};
