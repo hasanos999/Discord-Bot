ca1e226a2f017c783b0bebd800568cf1d57a2c66
Glitch (uptime2) <none>
2020-08-07 13:37:07 +0000

    🌷👫 Checkpoint ./komutlar/yardım2.js:6374285/25 ./bot.js:6374285/1614 ./komutlar/küfür.js:6374285/680

--- bot.js
+++ bot.js
@@ -940,3 +940,44 @@ client.on("guildMemberAdd", async member => {
    
 //
 
+client.on("message", async msg => {
+  
+  
+ const i = await db.fetch(`kufur_${msg.guild.id}`)
+    if (i == "acik") {
+        const kufur = ["oç", "amk", "ananı sikiyim", "ananıskm", "piç", "amk", "amsk", "sikim", "sikiyim", "orospu çocuğu", "piç kurusu", "kahpe", "orospu", "mal", "sik", "yarrak", "am", "amcık", "amık", "yarram", "sikimi ye", "mk", "mq", "aq", "ak", "amq",];
+        if (kufur.some(word => msg.content.includes(word))) {
+          try {
+            if (!msg.member.hasPermission("BAN_MEMBERS")) {
+                  msg.delete();
+                          
+                      return msg.reply('Bu Sunucuda Küfür Filtresi Aktiftir.')
+            }              
+          } catch(err) {
+            console.log(err);
+          }
+        }
+    }
+    if (!i) return;
+});
+
+client.on("messageUpdate", (oldMessage, newMessage) => {
+  
+  
+ const i = db.fetch(`${oldMessage.guild.id}.kufur`)
+    if (i) {
+        const kufur = ["oç", "amk", "ananı sikiyim", "ananıskm", "piç", "amk", "amsk", "sikim", "sikiyim", "orospu çocuğu", "piç kurusu", "kahpe", "orospu", "mal", "sik", "yarrak", "am", "amcık", "amık", "yarram", "sikimi ye", "mk", "mq", "aq", "ak", "amq",];
+        if (kufur.some(word => newMessage.content.includes(word))) {
+          try {
+            if (!oldMessage.member.hasPermission("BAN_MEMBERS")) {
+                  oldMessage.delete();
+                          
+                      return oldMessage.reply('Bu Sunucuda Küfür Filtresi Aktiftir.')
+            }              
+          } catch(err) {
+            console.log(err);
+          }
+        }
+    }
+    if (!i) return;
+});
--- komutlar/yardım2.js
+++ komutlar/yardım2.js
@@ -38,7 +38,7 @@ exports.run = (client, message, args) => {
         embed.setFooter(`Sayfa ${page} / ${pages.length}`)
         msg.edit(embed)
       })
-    message.react(remove)
+
       backwards.on('collect', r => {
         if(page === 1) return;
         page--;
