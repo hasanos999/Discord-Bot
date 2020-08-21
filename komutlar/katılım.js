d2eb6383db00bc157f49ece9e2782a718ea40618
Glitch (uptime2) <none>
2020-08-21 18:13:16 +0000

    🚛👚 Checkpoint ./komutlar/katılım.js:6374285/16321

--- komutlar/katılım.js
+++ komutlar/katılım.js
@@ -1,36 +1,12 @@
 const Discord = require('discord.js')
-const { RichEmbed } = require('discord.js')
-module.exports.run = async (client, message, args, channel) => {
 
- const Embed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
-channel.send(Embed);
-
-}
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['embed', 'embedyazı'],
-  permLevel: 0
+exports.run = (bot, message, args) => {  
+const pingUpdate = new Discord.MessageEmbed()
+.setColor('#0099ff')
+.setDescription('pinging...')
+message.channel.send(pingUpdate);
 }
 
 exports.help = {
-  name: 'embedyaz'
+  name: 'ping'
 }
