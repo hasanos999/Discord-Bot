e59d676915ebdb7890247e90f12fc4b32877011a
Glitch (uptime2) <none>
2020-08-21 17:50:46 +0000

    😄👽 Checkpoint ./komutlar/katılım.js:6374285/14369 ./.glitch-assets:6374285/375

--- komutlar/katılım.js
+++ komutlar/katılım.js
@@ -4,10 +4,17 @@ exports.run = (client, message, args) => {
 
   let yazıİçeriği = args.slice().join(' ')
   const Mesaj = new RichEmbed()
-    .setColor('RANDOM')
-    .setDescription(yazıi)
-    .setTitle('Link')
-    .setUrl('https://www.craftrise.tc/lonca/befa16a1-7800-4d5d-9b3c-0f6f4360e6d3')
+	.setColor('#0099ff')
+	.setTitle('PeeVed')
+	.setURL('https://www.craftrise.tc/lonca/befa16a1-7800-4d5d-9b3c-0f6f4360e6d3')
+	.setAuthor('Discord Linkimiz', 'https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598031841290', 'https://discord.gg/TTYsCaQ')
+	.setDescription('Bu Yazıya Tıklayarak Loncamızın Katılım Linkine Ulaşabilirsiniz.')
+	.setThumbnail('https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598031841290')
+	
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598031841290')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598031841290');
 
 message.channel.send(Mesaj)
 }
