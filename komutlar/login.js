6f45e4221837fec0b87b8df0a2bc8f8cc56c79d0
Glitch (uptime2) <none>
2020-08-21 10:40:24 +0000

    🎊🚖 Checkpoint ./komutlar/loncalogin.js:6374285/14 ./komutlar/kayıt.js:6374285/9491 ./komutlar/kayıt2.js:6374285/2265

--- komutlar/login.js
+++ komutlar/login.js
@@ -11,9 +11,9 @@ exports.run = async (client, message, args) => {
   if(!isim) return message.reply(`:warning: Lütfen bir kullanıcı adı giriniz! \nDoğru Kullanım; \`${prefix}isimdeğiştir @${client.user.username}#${client.user.discriminator} <yeni isim>\``).then(msg => msg.delete(5000))
   if(isim.length > 32) return message.reply(`:warning: Lütfen \`32\` karakteri geçmeyecek şekilde bir isim giriniz!`)
   message.guild.members.get(kullanici.id).setNickname(`${isim}`)
-  message.guild.members.get(kullanici.id).addRole("740551186687590490")//740560451628302356,
-  message.guild.members.get(kullanici.id).addRole("740560451628302356")
-  message.guild.members.get(kullanici.id).removeRole("740551290957856778")
+  message.guild.members.get(kullanici.id).addRole("745886985926737971")
+  message.guild.members.get(kullanici.id).addRole("745887207298039820")
+    message.guild.members.get(kullanici.id).removeRole("745887085113638922")
   message.channel.send(`:white_check_mark: Başarılı bir şekilde \`${kullanici.username}\` adlı kişinin kullanıcı adı \`${isim}\` olarak değiştirildi.`).then(msg => msg.delete(5000))
 }
 exports.conf = {
@@ -24,7 +24,7 @@ exports.conf = {
 }
 
 exports.help = {
-    name: 'loncalogin',
+    name: 'login',
     description: 'Belirttiğiniz kullanıcının kullanıcı adını değiştirir.',
     usage: 'isimdeğiştir @kullanıcı <kullanıcı adı>'
 }
