3d7114604fe2fd5db45b70dd2615e08c1693c6e4
Glitch (uptime2) <none>
2020-08-22 06:52:58 +0000

    ☄️👔 Checkpoint ./komutlar/katılım.js:6374285/1321

--- komutlar/katılım.js
+++ komutlar/katılım.js
@@ -9,10 +9,11 @@ exports.run = (client, message, args) => {
 	.setURL('https://www.craftrise.tc/lonca/befa16a1-7800-4d5d-9b3c-0f6f4360e6d3')
 	.setAuthor('PeeVed', 'https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598031841290', 'https://discord.gg/xsvNaq8')
 	.setDescription('"**Lonca**" yazısına tıklayarak lonca katılım linkine ulaşabilirsiniz.')
-	.setThumbnail('https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598078426940')
+	.setThumbnail('https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598031841290')
 
 
-	.addField('Yetkililer:', '**Mekktile**(Kurucu)\n', true)
+	.addField('\nYetkililer:', '**Mekktile**\n(Kurucu)\n\n**MiniAnn**\n(Moderatör)\n\n**YT_Eren**\n(Moderatör)\n\n**EndlessSpear**\n(Moderatör)\n\n**minesaus**\n(Moderatör)\n\n**missyhypnst**\n(Moderatör)\n\n**kocmar333**\n(Moderatör)\n\n**pufuu**\n(Moderatör)\n\n**Retuhn**\n(Moderatör)', true)
+  .addField('deneme','d', true)
 	.setImage('https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598031841290')
 	.setTimestamp()
 	.setFooter('Some footer text here', 'https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FPeeVed.jpg?v=1598077756090');
