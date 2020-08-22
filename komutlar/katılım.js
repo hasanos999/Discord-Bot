d6b205425d18d0b35da4e0fc7878f71bdc07bf06
Glitch (uptime2) <none>
2020-08-22 09:05:13 +0000

    ⛪️⛄️ Checkpoint ./komutlar/katılım.js:6374285/2505 ./komutlar/kurallar.js:6374285/9829 ./.glitch-assets:6374285/404

--- komutlar/katılım.js
+++ komutlar/katılım.js
@@ -14,7 +14,7 @@ exports.run = (client, message, args) => {
 
 	.addField('\nYetkililer:', '**Mekktile**\n(Kurucu)', true)
   .addField('Yetkililer:','\n**MiniAnn**\n(Moderatör)\n**YT_Eren**\n(Moderatör)\n**EndlessSpear**\n(Moderatör)\n**minesaus**\n(Moderatör)\n**missyhypnst**\n(Moderatör)\n**kocmar333**\n(Moderatör)\n**pufuu**\n(Moderatör)\n**Retuhn**\n(Moderatör)', true)
-	.setImage('https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598031841290')
+
 	.setTimestamp()
 	.setFooter('PeeVed', 'https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FPeeVed.jpg?v=1598077756090');
 
@@ -31,6 +31,7 @@ exports.conf = {
 }
 
 exports.help = {
-  name: '**!bilgi** ,'
+  name: '**!Bilgi** ,'
 
 }
+
