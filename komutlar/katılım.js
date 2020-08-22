279991ad8cff1783df885e0b7d3cea84e8a9873b
Glitch (uptime2) <none>
2020-08-22 14:33:44 +0000

    🌻🌼 Checkpoint ./komutlar/katılım.js:6374285/2988

--- komutlar/katılım.js
+++ komutlar/katılım.js
@@ -13,9 +13,9 @@ exports.run = (client, message, args) => {
 	.setThumbnail('https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598031841290')
 
 
-	.addField(`\nYetkililer:`, `**Mekktile** (Kurucu)\n**MiniAnn** (Moderatör)\n**YT_Eren** (Moderatör)\n**EndlessSpear** (Moderatör)\n**minesaus** (Moderatör)\n**missyhypnst** (Moderatör)\n**kocmar333** (Moderatör)\n**pufuu** (Moderatör)\n**Retuhn** (Moderatör)`, true)
-  .addField(`|` , `**|**\n**|**\n**|**\n**|**\n**|**\n**|**\n**|**\n**|**\n**|**`, true)
-  .addField(`Loncadaki Kullanıcı Sayısı: \n37 \n\n**Discorddaki Kullanıcı Sayısı:**\n \`, ${message.guild.memberCount}, \`\n\n**Aktif Yetkili Sayısı:**\n1` , true)
+	.addField('\nYetkililer:', '**Mekktile** (Kurucu)\n**MiniAnn** (Moderatör)\n**YT_Eren** (Moderatör)\n**EndlessSpear** (Moderatör)\n**minesaus** (Moderatör)\n**missyhypnst** (Moderatör)\n**kocmar333** (Moderatör)\n**pufuu** (Moderatör)\n**Retuhn** (Moderatör)', true)
+  .addField('|' , '**|**\n**|**\n**|**\n**|**\n**|**\n**|**\n**|**\n**|**\n**|**', true)
+  .addField('Discord Kullanıcı Sayısı: ' , `${message.guild.memberCount}`+' \n\n**Lonca Kullanıcı Sayısı:**\n45\n\n**Aktif Yetkili Sayısı:**\n3', true)
 	.setTimestamp()
 	.setFooter('PeeVed', 'https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FPeeVed.jpg?v=1598077756090');
 
@@ -27,12 +27,12 @@ exports.conf = {
   guildOnly: false,
   aliases: ['bilgi'],
   permLevel: 0,
-  kategori: "kullanıcı"
+ 
   
 }
 
 exports.help = {
-  name: '**!Bilgi** ,'
+  name: 'bilgi',
 
 }
 
