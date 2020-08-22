e0e5d0e83f3c35b50f5f5e86386a75b8310e7a51
Glitch (uptime2) <none>
2020-08-22 09:40:43 +0000

    👧🍏 Checkpoint ./komutlar/katılım.js:6374285/46

--- komutlar/katılım.js
+++ komutlar/katılım.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
   let yazıİçeriği = args.slice().join(' ')
   const Mesaj = new RichEmbed()
 	.setColor('#0099ff')
-	.setTitle('Lonca')
+	.setTitle('Lonca Davet Linki')
 	.setURL('https://www.craftrise.tc/lonca/befa16a1-7800-4d5d-9b3c-0f6f4360e6d3')
 	.setAuthor('PeeVed', 'https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598031841290', 'https://discord.gg/xsvNaq8')
 	.setThumbnail('https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FraccoonHead.png?v=1598031841290')
@@ -13,7 +13,7 @@ exports.run = (client, message, args) => {
 
 	.addField('\nYetkililer:', '**Mekktile** (Kurucu)\n**MiniAnn** (Moderatör)\n**YT_Eren** (Moderatör)\n**EndlessSpear** (Moderatör)\n**minesaus** (Moderatör)\n**missyhypnst** (Moderatör)\n**kocmar333** (Moderatör)\n**pufuu** (Moderatör)\n**Retuhn** (Moderatör)', true)
   .addField('|' , '**|**\n**|**\n**|**\n**|**\n**|**\n**|**\n**|**\n**|**\n**|**', true)
-  .addField('Loncadaki Kullanıcı Sayısı: ' , '37 \n\n**Discorddaki Kullanıcı Sayısı:**\nözel kod', true)
+  .addField('Loncadaki Kullanıcı Sayısı: ' , '37 \n\n**Discorddaki Kullanıcı Sayısı:**\nözel kod\n\n**Aktif Yetkili Sayısı:**\nkod', true)
 	.setTimestamp()
 	.setFooter('PeeVed', 'https://cdn.glitch.com/9e3a1417-6901-4041-baf5-d8644c0da566%2FPeeVed.jpg?v=1598077756090');
 
