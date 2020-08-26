73b14a1b55b7a52ec192eed856db904729c9ad6c
Glitch (puzzle-salty-stoat) <none>
2020-08-26 19:13:27 +0000

    🕋🐥 Checkpoint ./komutlar/temizle.js:6374285/6378 ./bot.js:6374285/62907 ./komutlar/forcaban.js:6374285/5728 ./komutlar/unban.js:6374285/2448 ./komutlar/banlist.js:6374285/1057

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -1,21 +1,27 @@
 const Discord = require('discord.js');
 exports.run = function(client, message, args) {
-if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanmak İçin İzniniz Yok!");
-if(!args[0]) return message.channel.send("🚫 **Lütfen Silinicek Mesaj Miktarını Yazın.!** 🚫");
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanmak İçin İzniniz Yok!").then(m => m.delete(8000))
+if(!args[0]) return message.channel.send("<:hayirr:495950887898841089> Lütfen Silinicek Mesaj Miktarını Yazın.!").then(m => m.delete(8000))
 message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(` ${args[0]} Adet Mesajı Sildim. ✅`).then(msg => msg.delete(5000));
+  const embed = new Discord.RichEmbed()
+       .setDescription(`Mesajlar Başarılı Bir Şekilde Yok Edildi.`)
+        .setImage(`https://tenor.com/view/konosoba-megumin-explosion-gif-7559840`)
+        .setFooter(`${message.author.username} Tarafından istendi.`)
+        .setColor("BLACK")
+        .setTimestamp()
+message.channel.send({ embed }).then(m => m.delete(8000))
 })
 }
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['sil'],
+  aliases: ['patlat','sil'],
   permLevel: 2
 };
 
 exports.help = {
   name: 'temizle',
   description: 'Belirlenen miktarda mesajı siler.',
-  usage: 'temizle <silinicek mesaj sayısı>'
+  usage: 'patlat <silinicek mesaj sayısı>'
 };
