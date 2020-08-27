29ff14ccdbe7349f4908663df758415cce7d62b2
hasanos999 <53007221+hasanos999@users.noreply.github.com>
2020-08-27 15:17:43 +0300

    Update ban.js

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -2,33 +2,33 @@ const Discord = require('discord.js');
 
 exports.run = async (bot, message, args) => {
       
-    if (!message.member.roles.find("name", "Yetkili")) {
-        return message.channel.send(' **Bu Komutu Kullanmak için** \*`Yetkili*\` **Rolüne Sahip Olman Lazım** ')
+    if (!message.member.roles.find("name", "Authorized")) {
+        return message.channel.send('** You Need To Have The ** \ * `Authorized * \` ** Role To Use This Command ** ')
             .then(m => m.delete(5000));
     } 
 const kisi = message.mentions.users.first()
 
 let reason = args.slice(1).join(' ')
-    if (!args[0]) return message.channel.send(":no_entry: Lütfen yasaklamak istediğiniz kullanıcıyı etiketleyiniz.")
+    if (!args[0]) return message.channel.send(":no_entry: Please tag the person you want to ban.")
     let user = message.mentions.users.first() || bot.users.get(args[0]) || message.guild.members.find(u => u.user.username.toLowerCase().includes(args[0].toLowerCase())).user
 
-    if (!user) return message.channel.send(`Etiketlediğiniz kullanıcıyı sunucuda bulamadım.`)
+    if (!user) return message.channel.send(`I could not find your tagged user on the server.`)
     let member = message.guild.member(user)
-    if (!member) return message.channel.send(`Etiketlediğiniz kullanıcıyı sunucuda bulamadım.`)
-    if (member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`Kendi yetkimin üstündeki kişileri yasaklayamam.`)
-    if (!reason) reason = 'Neden belirtilmemiş.'
+    if (!member) return message.channel.send(`I could not find your tagged user on the server.`)
+    if (member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`I can't ban people over my own jurisdiction.`)
+    if (!reason) reason = 'Why not specified.'
   
   message.guild.ban(kisi)
   
   let embed1 = new Discord.RichEmbed()
                 .setColor(0xffa300)
-                .addField(`${kisi} kişisi` + reason + "sebebi ile ", "yasaklandı.")
+                .addField(`User named${kisi}`,"was banned for " + reason + )//User named Ahmet was banned for crying
                 .setImage("https://cdn.discordapp.com/attachments/673224895756238848/673450899531628544/adalaett.gif")
                 .setFooter("Yasaklayan yetkili:", `${message.author.username}`);
   
    let embed = new Discord.RichEmbed()
                 .setColor(0xffa300)
-                .addField(`${kisi} kişisi ${reason} sebebi ile `, "yasaklandı.")
+                .addField(`User named${kisi}`,"was banned for " + reason + )
                 .setImage("https://cdn.discordapp.com/attachments/673224895756238848/673450899531628544/adalaett.gif");
                 bot.channels.get("693310347838226433").send(embed1).then(message.channel.send(embed))
 };
